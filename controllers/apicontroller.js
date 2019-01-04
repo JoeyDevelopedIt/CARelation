b77849fbb6034837bd95bc52c389055ac0459a6b
Joseph Adam <josephadam313@gmail.com>
2019-01-04 13:53:55 -0600

    added dynamic data

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -1,18 +1,14 @@
-// Basic Node application for requesting data from the OMDB website via axios
 // Here we incorporate the "axios" npm package
 var axios = require("axios");
 
 // We then run the request with axios module on a URL with a JSON
 function fetchCarMakes(year){
   //modify this api call to take a year and return a last of makes
-  axios.get("https://marketcheck-prod.apigee.net/v1/search?api_key=09oZb9G6v9CAkVxHvH2bApAWgXWACx4h&car_type=used&make=ford").then(
+  axios.get("https://marketcheck-prod.apigee.net/v1/search?api_key=09oZb9G6v9CAkVxHvH2bApAWgXWACx4h&car_type=toyota").then(
     function(response) {
-      // Then we print out the imdbRating
       // var data = JSON.stringify(response);
       console.log(response.data);
       
-  
-      // console.log("The movie's rating is: " + data);
     }
   );
 }
--- public/js/cars.js
+++ public/js/cars.js
@@ -9,4 +9,64 @@ $("#year").on("change", function() {
        //use jquery to update the next select options html with the list of car makes
        //repeat the whole process for each different option with different routes/api calls
    })
-})
+});
+
+$( document ).ready(function() {
+    $.get('/api/data')
+    .then(function(res){
+        for (var i=0; i<res.listings.length; i++){
+            console.log(res.listings[i].build.year);
+            //dynamically appending year of each car 
+            var years = document.createElement('option');
+            years.value = res.listings[i].build.year;
+            years.text = res.listings[i].build.year;
+            document.getElementById('year').appendChild(years);
+           // dynamically appening make of each car
+            var makedynamic = document.createElement('option');
+            makedynamic.value = res.listings[i].build.make;
+            makedynamic.text = res.listings[i].build.make;
+            document.getElementById('make').appendChild(makedynamic);    
+             // dynamically appening model of each car
+            var modeldynamic = document.createElement('option');
+            modeldynamic.value = res.listings[i].build.model;
+            modeldynamic.text = res.listings[i].build.model;
+            document.getElementById('model').appendChild(modeldynamic);    
+             // dynamically appening trim of each car
+            var trimdynamic = document.createElement('option');
+            trimdynamic.value = res.listings[i].build.trim;
+            trimdynamic.text = res.listings[i].build.trim;
+            document.getElementById('trim').appendChild(trimdynamic);                 
+             
+
+        }
+
+});
+
+
+
+// $("#searchbtn").on("click", function() {
+//     // var year = $(this).val();
+//     // console.log(year)
+//     //make an api call with the year
+//     $.get('/api/data')
+//     .then(function(res){
+//         for (var i=0; i<res.listings.length; i++){
+//             console.log(res.listings[i].build.year);
+//             var years = document.createElement('option');
+//             years.value = res.listings[i].build.year;
+//             years.text = res.listings[i].build.year;
+//             document.getElementById('year').appendChild(years);
+        
+
+        
+    
+//         }
+
+
+        // console.log(res)
+        // console.log(res.listings[0].build.year)
+        //should get a list of car makes
+        //use jquery to update the next select options html with the list of car makes
+        //repeat the whole process for each different option with different routes/api calls
+    // })
+ });
