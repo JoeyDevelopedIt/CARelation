661609bf770f82e559f5ba0959308d253d5dd9fe
Joseph Adam <josephadam313@gmail.com>
2019-01-07 13:16:00 -0600

    added new layout edits

--- public/js/cars.js
+++ public/js/cars.js
@@ -32,35 +32,6 @@ $("#vinSearch").on("click", function() {
     });
 })
  
- // $("#make").on("change", function() {
- //     var make = $(this).val();
- //     console.log(make)
- //     //make an api call with the make
- //     $.post('/api/data/make', {make: make})
- //     .then(function(res){
- //         console.log(res.make)
- //     })
- //  });
-  
- //  $("#model").on("change", function() {
- //     var model = $(this).val();
- //     console.log(model)
- //     //make an api call with the model
- //     $.post('/api/data/model', {model: model})
- //     .then(function(res){
- //         console.log(res.model)
- //     })
- //  });
- 
- //  $("#trim").on("change", function() {
- //     var trim = $(this).val();
- //     console.log(model)
- //     //make an api call with the model
- //     $.post('/api/data/trim', {trim: trim})
- //     .then(function(res){
- //         console.log(res.model)
- //     })
- //  });
  
  $(document).ready(function() {
      
@@ -95,52 +66,5 @@ $("#vinSearch").on("click", function() {
              document.getElementById('year').appendChild(years);
          }
  });
+ });
  
- 
- 
- // $("#searchbtn").on("click", function() {
- //     // var year = $(this).val();
- //     // console.log(year)
- //     //make an api call with the year
- //     $.get('/api/data')
- //     .then(function(res){
- //         for (var i=0; i<res.listings.length; i++){
- //             console.log(res.listings[i].build.year);
- //             var years = document.createElement('option');
- //             years.value = res.listings[i].build.year;
- //             years.text = res.listings[i].build.year;
- //             document.getElementById('year').appendChild(years);
-         
- 
-         
-     
- //         }
- 
- 
-         // console.log(res)
-         // console.log(res.listings[0].build.year)
-         //should get a list of car makes
-         //use jquery to update the next select options html with the list of car makes
-         //repeat the whole process for each different option with different routes/api calls
-     // })
-  });
- 
- 
- 
-         //     //dynamically appending year of each car 
-     
-         //    // dynamically appening make of each car
-         //     var makedynamic = document.createElement('option');
-         //     makedynamic.value = res.listings[i].build.make;
-         //     makedynamic.text = res.listings[i].build.make;
-         //     document.getElementById('make').appendChild(makedynamic);    
-         //      // dynamically appening model of each car
-         //     var modeldynamic = document.createElement('option');
-         //     modeldynamic.value = res.listings[i].build.model;
-         //     modeldynamic.text = res.listings[i].build.model;
-         //     document.getElementById('model').appendChild(modeldynamic);    
-         //      // dynamically appening trim of each car
-         //     var trimdynamic = document.createElement('option');
-         //     trimdynamic.value = res.listings[i].build.trim;
-         //     trimdynamic.text = res.listings[i].build.trim;
-         //     document.getElementById('trim').appendChild(trimdynamic);  
